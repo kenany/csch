f61036339ca878608df6c7accf05598ebdac0819
Kenan Yildirim <kenan@kenany.me>
2015-02-01 13:09:07 -0500

    Use `almost-equal` because of decimal differences between node and iojs
    This is probably caused by their different versions of V8.

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ var csch = require('../');
 var test = require('tape');
 var isFunction = require('lodash.isfunction');
 var isNaN = require('lodash.isnan');
+var almostEqual = require('almost-equal');
 
 test('exports a function', function(t) {
   t.plan(1);
@@ -11,10 +12,10 @@ test('exports a function', function(t) {
 test('csch', function(t) {
   t.plan(7);
   t.ok(isNaN(csch(0)));
-  t.equal(csch(1), 0.8509181282393216);
-  t.equal(csch(2), 0.27572056477178325);
-  t.equal(csch(3), 0.09982156966882273);
-  t.equal(csch(Math.PI), 0.08658953753004692);
-  t.equal(csch(1e-22), Number.POSITIVE_INFINITY);
-  t.equal(csch(-1e-22), Number.NEGATIVE_INFINITY);
+  t.ok(almostEqual(csch(1), 0.8509181282393216));
+  t.ok(almostEqual(csch(2), 0.27572056477178325));
+  t.ok(almostEqual(csch(3), 0.09982156966882273));
+  t.ok(almostEqual(csch(Math.PI), 0.08658953753004692));
+  t.ok(almostEqual(csch(1e-22), Number.POSITIVE_INFINITY));
+  t.ok(almostEqual(csch(-1e-22), Number.NEGATIVE_INFINITY));
 });
