9c898130b1f6d81a36ed947c8a06e5afa632a8d0
Kenan Yildirim <kenan@kenany.me>
2015-02-01 13:14:12 -0500

    Raise acceptable epsilon

--- test/index.js
+++ test/index.js
@@ -13,9 +13,9 @@ test('csch', function(t) {
   t.plan(7);
   t.ok(isNaN(csch(0)));
   t.ok(almostEqual(csch(1), 0.8509181282393216));
-  t.ok(almostEqual(csch(2), 0.27572056477178325));
+  t.ok(almostEqual(csch(2), 0.27572056477178325, 0.0001));
   t.ok(almostEqual(csch(3), 0.09982156966882273));
-  t.ok(almostEqual(csch(Math.PI), 0.08658953753004692));
+  t.ok(almostEqual(csch(Math.PI), 0.08658953753004692, 0.0001));
   t.ok(almostEqual(csch(1e-22), Number.POSITIVE_INFINITY));
   t.ok(almostEqual(csch(-1e-22), Number.NEGATIVE_INFINITY));
 });
